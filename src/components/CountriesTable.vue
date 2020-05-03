92818700366d1ac0896e79ea0345de7c016bd9ca
Tom Stark <admin@metallists.com>
2020-05-03 17:02:01 +0100

    'edit mode' - conditionally show input fields (or spans) and change input button.

--- src/components/CountriesTable.vue
+++ src/components/CountriesTable.vue
@@ -2,17 +2,25 @@
     <el-table :data="getAllCountries" :default-sort = "{ prop: 'name', order: 'ascending' }" style="width: 100%" stripe>
         <el-table-column label="Code" prop="code" width="90px" sortable>
             <template slot-scope="scope">
-                <span>{{ scope.row.code }}</span>
+                <div v-if="getIsRowInEditMode(scope)">
+                    <el-input placeholder="Code" v-model="codeInput"></el-input>
+                </div>
+                <span v-else>{{ scope.row.code }}</span>
             </template>
         </el-table-column>
         <el-table-column label="Name" prop="name" sortable>
             <template slot-scope="scope">
-                <span>{{ scope.row.name }}</span>
+                <div v-if="getIsRowInEditMode(scope)">
+                    <el-input placeholder="Country name" v-model="nameInput"></el-input>
+                </div>
+                <span v-else>{{ scope.row.name }}</span>
             </template>
         </el-table-column>
-        <el-table-column label="Operations" align="right" width="180px">
+        <el-table-column label="Operations" align="right" width="200px">
             <template slot-scope="scope">
-                <el-button size="mini" @click="handlerToDo(scope.$index, scope.row)">Edit</el-button>
+                <el-button :class="{ 'el-button--primary' : getIsRowInEditMode(scope) }" size="mini" @click="onClickEdit(scope.row)">
+                    <i class="el-icon-edit"></i> {{ getIsRowInEditMode(scope) ? 'Submit' : 'Edit' }}
+                </el-button>
                 <el-button size="mini" type="danger" @click="handlerToDo(scope.$index, scope.row)">Delete</el-button>
             </template>
         </el-table-column>
@@ -23,8 +31,27 @@
     import { mapGetters } from 'vuex';
     export default {
       name: 'CountriesTable',
+      data() {
+        return {
+          codeInput: '',
+          nameInput: '',
+          editingCountryCode: ''
+        }
+      },
       methods: {
-        // ToDo
+        getIsRowInEditMode(scope) {
+          return (scope.row.code === this.editingCountryCode);
+        },
+        onClickEdit({ code, name }) {
+          this.editingCountryCode = ((!this.editingCountryCode) ? code : ''); // toggle
+          if (!this.editingCountryCode) {
+            // submitting edit
+            /* eslint-disable */ console.log(`Test - SUBMITTING edit for country: ${name}`);
+            return;
+          }
+          this.codeInput = code;
+          this.nameInput = name;
+        }
       },
       computed: {
         ...mapGetters([ 'getAllCountries' ]),
