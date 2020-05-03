05f4ca9dfef4a0ba31b68bde382d94287b2bd58f
Tom Stark <admin@metallists.com>
2020-05-03 16:29:14 +0100

    Element UI container, header, main. Simple styles.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="app">
+    <el-container>
+      <el-header class="site-header"><p>Countries</p></el-header>
+      <el-main><p>Hello world</p></el-main>
+    </el-container>
   </div>
 </template>
 
 <script>
-
-export default {
-  name: 'app',
-  components: {
+  export default {
+    name: 'app',
+    components: {
+    }
   }
-}
 </script>
 
 <style lang="scss">
-
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: 'Helvetica', 'Arial', 'sans-serif';
+    line-height: 1.5;
+  }
+  .site-header {
+    background: #b3c0d1;
+    display: flex;
+    align-content: center;
+  }
 </style>
