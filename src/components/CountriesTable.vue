37e8e4c265524950f10fd14483a38785b6549cd6
Tom Stark <admin@metallists.com>
2020-05-05 18:41:13 +0100

    Validate edits, solves bug that arises due to duplicate data.

--- src/components/CountriesTable.vue
+++ src/components/CountriesTable.vue
@@ -6,10 +6,10 @@
         stripe>
         <el-table-column label="Code" prop="code" width="90px" sortable>
             <template slot-scope="scope">
-                <div v-if="getIsRowInEditMode(scope)">
+                <div v-if="isRowInEditMode(scope)">
                     <el-input
-                        @keyup.enter.native="submitEdit(scope.row.code)"
-                        @keyup.escape.native="exitEditWithoutSubmission(scope.row.code)"
+                        @keyup.enter.native="submitEdit(scope.row)"
+                        @keyup.escape.native="exitEditMode"
                         placeholder="Code"
                         v-model="codeInput"
                         ref="codeInput">
@@ -20,10 +20,10 @@
         </el-table-column>
         <el-table-column label="Name" prop="name" sortable>
             <template slot-scope="scope">
-                <div v-if="getIsRowInEditMode(scope)">
+                <div v-if="isRowInEditMode(scope)">
                     <el-input
-                        @keyup.enter.native="submitEdit(scope.row.code)"
-                        @keyup.escape.native="exitEditWithoutSubmission(scope.row.code)"
+                        @keyup.enter.native="submitEdit(scope.row)"
+                        @keyup.escape.native="exitEditMode"
                         placeholder="Country name"
                         v-model="nameInput">
                     </el-input>
@@ -35,10 +35,10 @@
             <template slot-scope="scope">
                 <el-button
                     @click="onClickEdit(scope.row)"
-                    :disabled="!getIsRowInEditMode(scope) && isInEditMode"
-                    :class="{ 'el-button--primary' : getIsRowInEditMode(scope) }"
+                    :disabled="!isRowInEditMode(scope) && isInEditMode"
+                    :class="{ 'el-button--primary' : isRowInEditMode(scope) }"
                     size="mini">
-                    <i class="el-icon-edit"></i> {{ getIsRowInEditMode(scope) ? 'Submit' : 'Edit' }}
+                    <i class="el-icon-edit"></i> {{ isRowInEditMode(scope) ? 'Submit' : 'Edit' }}
                 </el-button>
                 <el-button
                     @click="openDeleteConfirmModal(scope.row)"
@@ -60,7 +60,8 @@
         return {
           codeInput: '',
           nameInput: '',
-          editingCountryCode: ''
+          editingCountryCode: '',
+          validationErrors: []
         }
       },
       computed: {
@@ -68,9 +69,6 @@
         ...mapGetters([ 'getAllCountries' ]),
       },
       methods: {
-        getIsRowInEditMode(scope) {
-          return (scope.row.code === this.editingCountryCode);
-        },
         openDeleteConfirmModal({ code,name }) {
           this.$confirm(`Are you sure you want to delete ${name}?`, 'Warning', {
             confirmButtonText: 'Delete',
@@ -78,56 +76,97 @@
             type: 'warning'
           }).then(() => {
             this.$store.dispatch('deleteCountry', code);
-            this.$message({
-              type: 'success',
-              message: 'Delete completed'
-            });
+            this.$message.success('Delete completed');
           }).catch(() => {
-            this.$message({
-              type: 'info',
-              message: 'Delete canceled'
-            });
+            this.$message.info('Delete canceled');
           });
         },
         onClickEdit({ code, name }) {
+          // code and name are the ORIGINAL data (not the bound data)
           this.ensureCodeFieldFocused();
           this.$store.dispatch('setIsInEditMode', true);
-          this.editingCountryCode = ((!this.editingCountryCode) ? code : ''); // toggle
-          if (!this.editingCountryCode) {
-            if (this.isCodeOverTwoCharacaters(this.codeInput)) {
-              this.editingCountryCode = code; // maintain edit mode
-              return this.$message.error('Country code must be two characters in length.');
-            }
-            this.submitEdit(code);
+          this.setToggleEditingCountryCode(code);
+
+          if (!this.editingCountryCode) { // submitting
+            this.submitEdit({ code, name }, true);
             return;
           }
           this.codeInput = code;
           this.nameInput = name;
         },
-        submitEdit(originalCode) {
+        submitEdit(originalData, isFromEditButtonClick = false) {
+          const originalCode = originalData.code;
+          const originalName = originalData.name;
+          // ...validation guard clauses...
+          if (this.isNoChangeToData(originalCode, originalName)) {
+            // no need to re-submit identical data (save server resources)
+            this.exitEditMode();
+            return;
+          }
+          this.validateChanges(originalCode, originalName);
+          if (this.isValidationErrors()) {
+            if (isFromEditButtonClick) {
+              // maintains edit mode
+              this.setToggleEditingCountryCode(originalCode);
+            }
+            this.$message.error(this.validationErrors[0]);
+            this.validationErrors = [];
+            return;
+          }
+          // ...passed validation...
           this.$store.dispatch('submitCountryEdit', {
             originalCode,
             code: this.codeInput,
             name: this.nameInput
           });
-          this.$store.dispatch('setIsInEditMode', false);
-          this.editingCountryCode = '';
+          this.exitEditMode();
         },
-        isCodeOverTwoCharacaters() {
-          return this.codeInput.length !== 2;
+        validateChanges(originalCode, originalName) {
+          const _isCodeNotTwoCharacters = () => this.codeInput.length !== 2;
+          const _isCodeAlreadyUsed = (newCode) => {
+            const isCodeUsed = !!this.getAllCountries.find(country => country.code === newCode);
+            return isCodeUsed && (newCode !== originalCode);
+          };
+          const _isNameAlreadyUsed = (newName) => {
+            const isNameUsed = !!this.getAllCountries.find(country => country.name === newName);
+            return isNameUsed && (newName !== originalName)
+          };
+          if (_isCodeNotTwoCharacters(this.codeInput)) {
+            this.validationErrors.push('Country code must be two characters in length.');
+          }
+          if (_isCodeAlreadyUsed(this.codeInput)) {
+            this.validationErrors.push(`${this.codeInput} is already in use. Codes must be unique.`);
+          }
+          if (_isNameAlreadyUsed(this.nameInput)) {
+            this.validationErrors.push(`${this.nameInput} is already in use. Country names must be unique.`);
+          }
+        },
+        isValidationErrors() {
+          return !!this.validationErrors.length;
+        },
+        isRowInEditMode(scope) {
+          return (scope.row.code === this.editingCountryCode);
+        },
+        setToggleEditingCountryCode(code) {
+          this.editingCountryCode = ((!this.editingCountryCode) ? code : '');
+        },
+        isNoChangeToData(originalCode, originalName) {
+          const isNoChangeToCode = (originalCode === this.codeInput);
+          const isNoChangeToName = (originalName === this.nameInput);
+          return isNoChangeToCode && isNoChangeToName;
         },
         ensureCodeFieldFocused() {
-            // This is admittedly ugly when mixed with Element UI
-            setTimeout(() => {
-              const codeInputElement = this.$refs.codeInput;
-              if (codeInputElement) {
-                codeInputElement.$el.children[0].focus();
-              }
-            }, 1);
+          // This is admittedly ugly when mixed with Element UI
+          setTimeout(() => {
+            const codeInputElement = this.$refs.codeInput;
+            if (codeInputElement) {
+              codeInputElement.$el.children[0].focus();
+            }
+          }, 1);
         },
-        exitEditWithoutSubmission() {
-          this.editingCountryCode = '';
+        exitEditMode() {
           this.$store.dispatch('setIsInEditMode', false);
+          this.editingCountryCode = '';
         }
       }
     }
